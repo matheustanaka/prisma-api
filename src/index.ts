d9efb170295862161a9329ac72d83df79cefc2e0
Matheus Tanaka <matheustanakanog@gmail.com>
2022-10-19 19:39:23 -0300

    feat: add route to get by id

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,19 @@ app.get('/orders', async(req, res) => {
     res.json(orders)
 })
 
+//Get order by id
+app.get('/orders/:id', async(req, res) => {
+    const { id } = req.params;
+    
+    const orderById = await prisma.order.findUnique({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    res.json(orderById)
+})
+
 app.listen(3000, () =>
   console.log('REST API server ready at: http://localhost:3000'),
 )
