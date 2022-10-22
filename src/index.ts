5be412a0f91aadebb9ee999d4d73f74729daac15
Matheus Tanaka <matheustanakanog@gmail.com>
2022-10-21 23:47:03 -0300

    feat: add return status code

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,180 @@
-import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
 import express from 'express'
+import cors from 'cors'
 
-const prisma = new PrismaClient({
-    log: ['query'],
-})
+import { prisma } from './database/database'
+import { Order } from './types/order'
+
 const app = express()
-
 app.use(express.json())
+app.use(cors())
 
 //Get all orders
-app.get('/orders', async(req, res) => {
-    const orders = await prisma.order.findMany()
+app.get('/orders', async(req: Request, res: Response) => {
+    try {
+        const orders = await prisma.order.findMany({
+            select: {
+                id: true,
+                product: true,
+                seller: true,
+                country: true,
+                price: true
+            }
+        })
 
-    res.json(orders)
+        if(!orders) {
+            return res.status(404).json({ error: "Not found the orders"})
+        }
+
+        return res.status(200).json(orders)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
+
 })
 
 //Get order by id
-app.get('/orders/:id', async(req, res) => {
-    const { id } = req.params;
+app.get('/orders/:id', async(req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
     
-    const orderById = await prisma.order.findUnique({
-        where: {
-            id: Number(id)
-        }
-    })
+        const orderById = await prisma.order.findUnique({
+            where: {
+                id: Number(id)
+            },
+            select: {
+                id: true,
+                product: true,
+                seller: true,
+                country: true,
+                price: true
+            }
+        })
 
-    res.json(orderById)
+        if(!orderById) {
+            return res.status(404).json({ error: "Not found the Order"})
+        }
+
+        return res.status(200).json(orderById)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
 })
 
 //Get order by country
-app.get('/order/:country', async(req, res) => {
-    const { country } = req.params;
-    
-    const filterByCountry = await prisma.order.findMany({
-        where: {
-            country: {
-                search: country
+app.get('/order/:country', async(req: Request, res: Response) => {
+    try {
+        const { country } = req.params;
+        
+        const filterByCountry = await prisma.order.findMany({
+            where: {
+                country: {
+                    search: country
+                }
+            },
+            orderBy: {
+                id: 'asc'
             }
-        },
-        orderBy: {
-            id: 'asc'
-        }
-    })
+        })
 
-    res.json(filterByCountry)
+        if(!filterByCountry) {
+            return res.status(404).json({ error: "Not found the Country"})
+        }
+    
+        return res.status(200).json(filterByCountry)
+        
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
 })
 
 //Get order by seller
-app.get('/order/filter/:seller', async(req, res) => {
-    const { seller } = req.params;
-    
-    const filterBySeller = await prisma.order.findMany({
-        where: {
-            seller: { 
-                in: seller
-             }
-        },
-        orderBy: {
-            id: 'asc'
-        }
-    })
-    res.json(filterBySeller)
+app.get('/order/filter/:seller', async(req: Request, res: Response) => {
+    try {
+        const { seller } = req.params;
 
+        const filterBySeller = await prisma.order.findMany({
+            where: {
+                seller: { 
+                    in: seller
+                }
+            },
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        if(!filterBySeller) {
+            return res.status(404).json({ error: "Not found the Seller"})
+        }
+
+        return res.status(200).json(filterBySeller)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
 })
 
 //Create Order
-app.post('/order', async (req, res) => {
-    const createOrder = await prisma.order.create({
-        data: {
-            ...req.body
-        }
-    })
-
-    res.json(createOrder)
+app.post('/order', async (req: Request, res: Response) => {
+    try {
+        const createOrder = await prisma.order.create({
+            data: {
+                ...req.body
+            }
+        })
+    
+        return res.status(201).json(createOrder)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
+    
 })
 
 //Update order
-app.put('/order/update/:id', async(req, res) => {
-    const { id } = req.params;
+app.put('/order/update/:id', async(req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
 
-    const updateOrder = await prisma.order.update({
-        where: {
-            id: Number(id)
-        },
-        data: {
-            ...req.body
+        const updateOrder = await prisma.order.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                ...req.body
+            }
+        })
+    
+        if(!updateOrder) {
+            return res.status(404).json({ error: "Not found the Order to Update"})
         }
-    })
+        
+        return res.status(200).json(updateOrder)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
 
-    res.json(updateOrder)
 })
 
 //Delete order
-app.delete('/order/:id', async (req, res) => {
-    const { id } = req.params;
+app.delete('/order/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
 
-    const deleteOrder = await prisma.order.delete({
-        where: {
-            id: Number(id)
+        const deleteOrder = await prisma.order.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if(!deleteOrder) {
+            return res.status(404).json({ error: "Not found the Order to Delete"})
         }
-    })
 
-    res.json(deleteOrder)
+        return res.status(200).json(deleteOrder)
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"})
+    }
+    
 })
 
 app.listen(3000, () =>
